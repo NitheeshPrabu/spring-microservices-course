d144a60a637025b6cddae49a21b798804458f81c
Nitheesh Prabu Shanmugasundaram <nitheesh.prabu.shanmugasundaram@navis.com>
2020-09-13 18:04:14 +0530

    added ribbon load balancer

--- microservices/currency-conversion-service/src/main/java/com/example/microservices/currencyconversionservice/proxy/CurrencyExchangeServiceProxy.java
+++ microservices/currency-conversion-service/src/main/java/com/example/microservices/currencyconversionservice/proxy/CurrencyExchangeServiceProxy.java
@@ -1,11 +1,14 @@
 package com.example.microservices.currencyconversionservice.proxy;
 
 import com.example.microservices.currencyconversionservice.bean.CurrencyConversion;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-@FeignClient(name = "currency-exchange-service", url = "localhost:8000")
+//@FeignClient(name = "currency-exchange-service", url = "localhost:8000")
+@FeignClient(name = "currency-exchange-service") // no need to add url with ribbon - client side load balancer
+@RibbonClient(name = "currency-exchange-service")
 public interface CurrencyExchangeServiceProxy {
     @GetMapping(path = "/currency-exchange")
     public CurrencyConversion getExchangeValue(@RequestParam String from, @RequestParam String to);
